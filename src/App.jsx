1f9520f182109c8bcb2ce0539e023de6f6825acd
Simon <krenich.szymon@gmail.com>
2023-06-04 18:42:22 +0200

    fetching data, selecting it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.scss'
 import './index.css'
 import Home from './components/Home';
 import Result from './components/Result';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState(false);
+  const [countriesData, setCountriesData] = useState(null);
 
+
+  useEffect(() => {
+    async function getAllCountries() {
+      const url = `https://restcountries.com/v3.1/region/europe?fields=name,capital,currencies`;
+      const requestParams = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+  
+      try {
+        const response = await fetch(url, requestParams);
+        const data = await response.json();
+
+        const countryCapitalPairs = data.reduce((accum, current) => {
+          if (current.name && current.capital) {
+            accum[current.name.common] = current.capital[0];
+          }
+          return accum;
+        }, {});
+  
+        console.log(countryCapitalPairs);
+        setCountriesData(countryCapitalPairs);
+        console.log("Fetched Data successfuly");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
+    getAllCountries();
+  
+  }, []);
+  
   return (
     <>
       <div className="app">
@@ -18,8 +53,12 @@ function App() {
           </div>
 
           <div className="content">
-            <Home></Home>
+            {result ? 
+                <Result setResult={setResult} /> : 
+                countriesData && <Home countriesData={countriesData} />
+            }
           </div>
+
         </div>
       </div>
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,70 @@
+import { useEffect, useState } from 'react';
 import classes from './Home.module.scss'
 
-function Home() {
+function Home({countriesData}) {
+  
+  const [capital, setCapital] = useState("");
+  const [rightCountry, setRightCountry] = useState("");
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    function getRandomAnswers() {
+        const countries = Object.keys(countriesData);
+        let list = [];
+        let correctAnswer; // Define correctAnswer
+    
+        // get 4 random countries
+        while (list.length < 4) {
+          const randomIndex = Math.floor(Math.random() * countries.length);
+          if (!list.includes(countries[randomIndex])) {
+            list.push(countries[randomIndex]);
+            console.log(countries[randomIndex]);
+          }
+        }
+    
+        // get a random country from the list
+        correctAnswer = list[Math.floor(Math.random() * list.length)]; 
+    
+        // set states
+        setAnswers(list);
+        setRightCountry(correctAnswer); 
+        setCapital(countriesData[correctAnswer]);
+    }
+
+    if (countriesData && answers.length === 0) {
+      getRandomAnswers();
+    }
+
+  }, [countriesData]); // Removed answers.length as a dependency
+   
+  
 
   return (
     <div className={classes.wrapper}>
       <div className={classes.head}>
-        <h2>Kuala lumpur is the capitol of</h2>
+        <h2>{capital} is the capitol of</h2>
       </div>
 
       <div className={classes.answers}>
         <ul>
-            <li><span className={classes.highlight}>A</span>Vietnam</li>
-            <li><span className={classes.highlight}>B</span>Malaysia</li>
-            <li><span className={classes.highlight}>C</span>Sweden</li>
-            <li><span className={classes.highlight}>D</span>Poland</li>
+          {answers.map((country, index) => { // Included index as a parameter
+            const options = ["A", "B", "C", "D"];
+            return (
+              <li key={index}>
+                <span className={classes.highlight}>{options[index]}</span>{country}
+              </li>
+            );
+          })}
         </ul>
 
         <div className={classes.btn}>
-          <input type="submit" value="Next" />
+          <input type="submit" value="Next"/>
         </div>
-
+      
       </div>  
     </div>
   )
 }
+
 
 export default Home;
